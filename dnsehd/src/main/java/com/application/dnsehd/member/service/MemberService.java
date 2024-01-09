757bf2b5ae554e0d70d31220fc17a3e57078b3e0
fusiontwo <yr0955@naver.com>
2024-01-09 21:02:58 +0900

    .

--- dnsehd/src/main/java/com/application/dnsehd/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.application.dnsehd.member.service;
-
-import com.application.dnsehd.member.dto.MemberDTO;
-
-public interface MemberService {
-
-	public void addMember(MemberDTO memberDTO);
-	public String checkValidId(String memberId);
-}
